7c6327a551912bf45f8ea482db8e37c5f3cc5b95
MitchelHdz <sebastianvicky_@hotmail.com>
2015-06-24 18:45:23 -0500

    Work on Frontend

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,11 @@ router.get('/index', function(req, res, next) {
   res.render('users_ind', { title: 'Amoxtli' });
 });
 router.get('/reports', function(req, res, next) {
-  res.render('report', { title: 'Reportes' });
+  res.render('report', { title: 'Reportes', 'lendings':
+	[
+		{ 'id_user': '1', 'id_book': '1', 'date': 'hoy'},
+		{ 'id_user': '2', 'id_book': '2', 'date': 'mañana'}
+	]});
 });
 router.get('/lendings', function(req, res, next) {
   res.render('lending', { title: 'Prestamos' ,	'lendings':
